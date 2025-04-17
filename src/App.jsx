fc4890f46e7ce15883f6ab8b1506121491b0c188
s4mp41xao <samribe@outlook.com>
2025-04-17 11:51:08 -0300

    build: start project with react and vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AddTask from './components/AddTask'
+import Tasks from './components/Tasks'
+
+function App() {
+  return (
+    <div>
+      <h1>Gerenciador de Tarefas</h1>
+      <Tasks />
+      <AddTask />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,9 @@
+function AddTask() {
+  return (
+    <div>
+      <h1>Adicionar Tarefa</h1>
+    </div>
+  )
+}
+
+export default AddTask
