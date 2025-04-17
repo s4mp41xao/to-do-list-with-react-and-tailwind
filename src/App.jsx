c5e5f914185d1893857c435dea726bb28dc646df
s4mp41xao <samuelpaixao@proton.me>
2025-04-17 17:40:15 -0300

    feat: add Task component to include and remove item from the list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
+import { useState } from 'react'
 import AddTask from './components/AddTask'
 import Tasks from './components/Tasks'
 
 function App() {
+  // const [tasks deleteTask] = useState()
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Comprar Refrigerante',
+      description: 'Marca mais barata',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      title: 'Comprar Pão',
+      description: 'Marca mais barata',
+      isCompleted: false
+    }
+  ])
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map(task => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted }
+      }
+      return task
+    })
+    setTasks(newTasks)
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter(task => task.id != taskId)
+    setTasks(newTasks)
+  }
+
   return (
-    <div>
-      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
-      <Tasks />
-      <AddTask />
+    <div className="w-screen h-screen flex justify-center p-6">
+      <div className="w-[500px] border-4 p-4 rounded-md border-black bg-gray-100">
+        <h1 className="text-3xl font-bold text-center p-3">
+          Gerenciador de Tarefas
+        </h1>
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+        <AddTask />
+      </div>
     </div>
   )
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,35 @@
-function Tasks() {
+import { ChevronRightIcon, Trash2 } from 'lucide-react'
+
+function Tasks(props) {
+  // console.log(props)
+
   return (
     <div>
-      <h1>Tarefas</h1>
+      <ul className="space-y-4 p-6 bg-gray-200 rounded-md shadow-md">
+        {props.tasks.map(task => (
+          <li key={task.id} className="flex gap-2">
+            <button
+              onClick={() => props.onTaskClick(task.id)}
+              className={`w-full p-2 text-left bg-gray-300 rounded-md ${
+                task.isCompleted && 'line-through'
+              }`}
+            >
+              {task.title}
+            </button>
+
+            <button className="p-2 bg-gray-300 rounded-md">
+              <ChevronRightIcon />
+            </button>
+
+            <button
+              onClick={() => props.onDeleteTaskClick(task.id)}
+              className="p-2 bg-gray-300 rounded-md"
+            >
+              <Trash2 />
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
