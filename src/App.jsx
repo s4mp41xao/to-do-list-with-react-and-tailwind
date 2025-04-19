25afddcbad597a89229e0316f120f17ec7c94f08
s4mp41xao <samuelpaixao@proton.me>
2025-04-19 17:12:51 -0300

    feat: save tasks on localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AddTask from './components/AddTask'
 import Tasks from './components/Tasks'
 import { v4 } from 'uuid'
@@ -6,20 +6,13 @@ import { v4 } from 'uuid'
 function App() {
   // const [tasks deleteTask] = useState()
 
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: 'Comprar Refrigerante',
-      description: 'Marca mais barata',
-      isCompleted: false
-    },
-    {
-      id: 2,
-      title: 'Comprar Pão',
-      description: 'Marca mais barata',
-      isCompleted: false
-    }
-  ])
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem('tasks')) || []
+  )
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks])
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map(task => {
